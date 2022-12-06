a79b1e1756e22814156a330caba1c8832e93dcee
Siavash <siamhb7@protonmail.com>
2022-12-06 14:28:48 +0330

    simple functionality with render and createElement that render an React Element in the dom

--- src/react-dom/render/render.ts
+++ src/react-dom/render/render.ts
@@ -1,3 +1,25 @@
-export const render = () => {
+import type { ReactElement,  ReactTextElement } from "../../react/createElement/createElement"  
   
+const render = (element:ReactElement|ReactTextElement , dom:HTMLElement) => {
+
+  const child:any = element.type === "TEXT_ELEMENT" 
+  ?document.createTextNode("")
+  :document.createElement(element.type)
+
+  const isProperty = (key:string) => key !== "children"
+
+  Object.keys(element.props)
+  .filter(isProperty)
+  .forEach((prop)=>{
+    child[prop] = element.props[prop]
+  })
+
+  element.props.children.forEach((ch)=>{
+    render(ch , child)
+  })
+
+  dom.appendChild(child)
+
 }
+
+export {render}
--- src/react/createElement/createElement.ts
+++ src/react/createElement/createElement.ts
@@ -1,3 +1,43 @@
-export const createElement = () => {
-  
+export interface ReactElement {
+  type:string ;
+  props:{
+    [s:string]:any,
+    children:(ReactTextElement|ReactElement)[]
+  }
 }
+
+export interface ReactTextElement extends ReactElement {
+  type:"TEXT_ELEMENT"
+  props:{
+    [s:string]:any,
+    nodeValue:string,
+    children:[]
+  }
+}
+
+const textElement = (nodeValue:string):ReactTextElement =>{
+
+  return {
+    type:"TEXT_ELEMENT",
+    props:{
+      nodeValue:nodeValue,
+      children:[]
+    }
+  }
+}
+
+
+
+const createElement = (type:string , config:any , ...children:(string|ReactElement)[]):ReactElement => {
+  return {
+    type,
+    props:{
+      ...config,
+      children:children.map((ch)=>
+      typeof ch === 'object' ? ch : textElement(ch)
+      )
+    }
+  }
+}
+
+export {createElement}
